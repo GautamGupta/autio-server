f074cfb0a156b3271a9d5d214f1bb275a65403eb
Qasim Iqbal <me@qas.im>
2015-01-17 13:17:59 -0500

    Fix

--- routes/session.js
+++ routes/session.js
@@ -4,11 +4,17 @@ var router = express.Router();
 router.get('/', function(req, res) {
 
   if(req.query['action'] == "create") {
+
     res.json({"thing":"create session"})
+
   } else if(req.query['action'] == "join") {
+
     res.json({"thing":"join session"})
+
   } else if (req.query['action'] == "enqueue") {
+
     res.json({"thing":"enqueue track"})
+
   }
 
 });
