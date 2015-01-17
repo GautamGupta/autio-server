2ea47cc8b9de95a36e99a1e2f741a138df83314b
Qasim Iqbal <me@qas.im>
2015-01-17 10:00:55 -0500

    Implemented Spotify search

---
+++ routes/search.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var request = require('request');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+
+  request('https://api.spotify.com/v1/search?q=' + req.query['q'] + '&type=track&market=US&limit=10',
+  function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body)
+      var tracks = data.tracks.items
+      var response = []
+      for(var i = 0; i < tracks.length; i++) {
+        response.push({
+          "id": tracks[i].id,
+          "name": tracks[i].name,
+          "artist": tracks[i].artists[0].name,
+          "artwork": tracks[i].album.images[0]
+        })
+      }
+      res.json(response)
+    } else {
+      console.log(error)
+      res.status(403)
+    }
+  })
+
+});
+
+module.exports = router;
